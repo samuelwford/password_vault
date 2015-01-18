5e0c4fe62e644cf6992e9cd0014b13e19ffd1f7e
Samuel Ford <samuel.ford@icloud.com>
2015-01-18 09:32:16 -0500

    fix mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Pick up the application host name from the environment.
+  config.action_mailer.default_url_options = { :host => ENV["MAILER_HOST"] }
 end
