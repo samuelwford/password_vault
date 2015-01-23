8c50953b0270bc5810b1bc1c9a0e3580529cc985
Samuel Ford <samuel.ford@icloud.com>
2015-01-23 12:32:08 -0500

    fix busted authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  validates :email, presence: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
   
   has_and_belongs_to_many :groups
-  
-  def authenticate(password)
-    true
-  end
 end
