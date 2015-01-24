226eb7b51b7dbbbf769041d6f5c0c6facb13d4cc
Samuel Ford <samuel.ford@icloud.com>
2015-01-23 20:09:35 -0500

    clean up a little

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,45 +18,32 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_path(@user), notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to user_path(@user), notice: 'User was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_path(@user), notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'User was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
       authorize @user
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       if current_user.admin
         params.require(:user).permit(:name, :email, :url, :password, :password_confirmation, :admin, { group_ids: [] })
--- app/views/accounts/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @account, :id, :name, :password, :notes, :group_id, :account_name, :url, :created_at, :updated_at
