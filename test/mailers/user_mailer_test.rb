a343e6494ae837e49ecc1db8a2e1bb5bf692a989
Samuel Ford <samuel.ford@icloud.com>
2015-01-25 10:22:38 -0500

    one day i'll remember to run tests *before* i push ...

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,12 +1,17 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+  setup do
+    @user = users(:one)
+  end
+  
   test "password_reset" do
-    mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    token = User.new_token
+    mail = UserMailer.password_reset(@user, token)
+    assert_equal "Password Reset", mail.subject
+    assert_equal [@user.email], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match token, mail.body.encoded
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ class ActiveSupport::TestCase
   fixtures :all
   
   def sign_in(user)
-    remember_token = User.new_remember_token
+    remember_token = User.new_token
     cookies[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
   end
