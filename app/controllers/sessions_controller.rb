4d8cc9144bb443fe1b25a5c4e023fee199f3bf0b
Samuel Ford <samuel.ford@icloud.com>
2015-01-25 07:30:10 -0500

    add a remember me checkbox to signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
-      sign_in user
+      sign_in user, params[:remember_me]
       redirect_back_or user
     else
       flash.now.alert = "Invalid email or password."
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,11 @@
 module SessionsHelper
-  def sign_in(user)
+  def sign_in(user, permanent=false)
     remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
+    if permanent
+      cookies.permanent[:remember_token] = remember_token
+    else
+      cookies[:remember_token] = remember_token
+    end
     user.update_attribute(:remember_token, User.digest(remember_token))
     self.current_user = user
   end
