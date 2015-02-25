31860ed05cb8d7ed021126184940c97af7490a75
Samuel Ford <samuel.ford@icloud.com>
2015-02-25 15:59:26 -0500

    add import rake task, tweak account list for large list

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ActiveRecord::Base
   
   has_many :accounts
   has_and_belongs_to_many :users
+  
+  def self.find_or_create_by_name(name)
+    group = Group.where(name: name).first
+    if group.nil?
+      group = Group.new(name: name)
+    end
+    group
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -39,11 +39,13 @@ class User < ActiveRecord::Base
         password = User.new_token
         email_suffix_restriction = Rails.configuration.restrict_oauth_to_emails_suffix
         if data["email"].end_with?(email_suffix_restriction)
+          first_user = User.all.count == 0
           user = User.create(name: data["name"],
              email: data["email"],
              password: password,
              password_confirmation: password,
-             active: true
+             active: true,
+             admin: first_user ? true : false
           )
         end
       end
